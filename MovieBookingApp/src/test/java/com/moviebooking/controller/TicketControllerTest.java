4a951c84b0384ac5d2488337530f89ebb3c5d4d7
vagdevinanduri2001 <bhuvanananduri2001@gmail.com>
2023-04-29 23:44:08 +0530

    controller created just to test

---
+++ MovieBookingApp/src/test/java/com/moviebooking/controller/TicketControllerTest.java
@@ -0,0 +1,10 @@
+package com.moviebooking.controller;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@RunWith(MockitoJUnitRunner.class)
+@WebMvcTest
+public class TicketControllerTest {
+}
